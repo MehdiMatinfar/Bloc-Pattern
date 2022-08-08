890f18dc15c19b2f356916b5171dc0efff6f2e78
MehdiMatinfar <mehdi.matinfar1397@gmail.com>
2022-08-08 15:09:41 +0430

    States

--- lib/core/resources/data_state.dart
+++ lib/core/resources/data_state.dart
@@ -1,20 +1,18 @@
-abstract class DataState<T>{
-
+abstract class DataState<T> {
   final T? data;
   final String? error;
 
   DataState(this.data, this.error);
-
 }
 
-class DataStateSuccessfully<T> extends DataState<T>{
+class DataStateSuccessfully<T> extends DataState<T> {
   T? data;
 
-
-  DataStateSuccessfully({this.data}):super(data,null);
+  DataStateSuccessfully({this.data}) : super(data, null);
 }
-class DataStateError<T> extends DataState<T>{
+
+class DataStateError<T> extends DataState<T> {
   String? error;
 
-  DataStateError({this.error}):super(null,error);
+  DataStateError({this.error}) : super(null, error);
 }
