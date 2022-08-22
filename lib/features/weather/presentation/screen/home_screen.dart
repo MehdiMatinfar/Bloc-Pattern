9440087a4725a1401a499d3ce8d672991cf859e5
MehdiMatinfar <mehdi.matinfar1397@gmail.com>
2022-08-22 16:01:29 +0430

    setUp UI get current weather

--- lib/features/weather/presentation/screen/home_screen.dart
+++ lib/features/weather/presentation/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:weather_app/core/widgets/app_background.dart';
 import 'package:weather_app/core/widgets/spinkit.dart';
 import 'package:weather_app/features/weather/domain/entity/current_city_entity.dart';
@@ -15,10 +16,18 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     BlocProvider.of<HomeBloc>(context)
         .add(LoadCurrentWeatherEvent(cityName: 'Tehran'));
-
+    var height = MediaQuery
+        .of(context)
+        .size
+        .height;
+    var width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    PageController _pageController = PageController();
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height,
+        height: height,
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AppBackgroundImage.getCurrentAppBackground(),
@@ -32,42 +41,155 @@ class HomeScreen extends StatelessWidget {
                     child: LoaderIndicator(),
                   );
                 } else if (state.currentWeatherState
-                    is CurrentWeatherCompleted) {
+                is CurrentWeatherCompleted) {
                   final CurrentWeatherCompleted currentWeatherCompleted =
-                      state.currentWeatherState as CurrentWeatherCompleted;
+                  state.currentWeatherState as CurrentWeatherCompleted;
                   final CurrentCityEntity currentCityEntity =
-                      currentWeatherCompleted.currentCityEntity!;
+                  currentWeatherCompleted.currentCityEntity!;
                   return Expanded(
                       child: ListView(
-                    children: [
-                      Column(
                         children: [
                           Padding(
-                            padding: EdgeInsets.only(top: 50),
-                            child: Text(
-                              currentCityEntity.name!,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 24),
+                            padding: EdgeInsets.only(top: height * 0.02),
+                            child: SizedBox(
+                              width: width,
+                              height: height * 0.5,
+                              child: PageView.builder(
+                                itemBuilder: (context, index) {
+                                  if (index == 0) {
+                                    return Column(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 50),
+                                          child: Text(
+                                            currentCityEntity.name!,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: Text(
+                                            // 0 -> Current Weather
+                                            // 1 -> Tomorrow
+                                            currentCityEntity
+                                                .weather![0].description!,
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: WeatherImages.setIconForMain(
+                                              currentCityEntity
+                                                  .weather![0].description!),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: Text(
+                                            // 0 -> Current Weather
+                                            // 1 -> Tomorrow
+                                            "${currentCityEntity.main!.temp!
+                                                .round()}\u00B0",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 20),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                            children: [
+                                              Column(
+                                                children: [
+                                                  Text(
+                                                    'max',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    // 0 -> Current Weather
+                                                    // 1 -> Tomorrow
+                                                    "${currentCityEntity.main!
+                                                        .tempMax!
+                                                        .round()}\u00B0",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 24),
+                                                  )
+                                                ],
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(
+                                                    horizontal: 10),
+                                                child: Container(
+                                                  width: 1,
+                                                  height: 40,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                              Column(
+                                                children: [
+                                                  Text(
+                                                    'min',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    // 0 -> Current Weather
+                                                    // 1 -> Tomorrow
+                                                    "${currentCityEntity.main!
+                                                        .tempMin!
+                                                        .round()}\u00B0",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 24),
+                                                  )
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    );
+                                  } else {
+                                    return Container(
+                                      color: Colors.yellow,
+                                    );
+                                  }
+                                },
+                                physics: const AlwaysScrollableScrollPhysics(),
+                                allowImplicitScrolling: true,
+                                itemCount: 2,
+                                controller: _pageController,
+                              ),
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.only(top: 20),
-                            child: Text(
-                              // 0 -> Current Weather
-                              // 1 -> Tomorrow
-                              currentCityEntity.weather![0].description!,
-                              style:
-                              TextStyle(color: Colors.grey, fontSize: 24),
+                            padding: EdgeInsets.only(top: 10),
+                            child: Center(
+                              child: SmoothPageIndicator(
+                                controller: _pageController,
+                                count: 2,
+                                effect: const ExpandingDotsEffect(
+                                    dotWidth: 10,
+                                    dotHeight: 10,
+                                    spacing: 5,
+                                    activeDotColor: Colors.white),
+                                onDotClicked: (index) =>
+                                   _pageController.animateToPage(index,
+                                      duration: Duration(microseconds: 500),
+                                      curve: Curves.bounceOut)
+                                ,
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: 20),
-                            child: WeatherImages.setIconForMain(currentCityEntity.weather![0].description!),
                           )
                         ],
-                      )
-                    ],
-                  ));
+                      ));
                 } else if (state.currentWeatherState is CurrentWeatherError) {
                   return const Center(
                     child: Text('Error!'),
